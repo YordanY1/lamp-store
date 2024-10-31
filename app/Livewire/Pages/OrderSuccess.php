cf0544130466b3ab881b61df206ae0747567d734
YordanYordanov <yordanyordanov0104@gmail.com>
2024-10-31 11:38:15 +0200

    stripe added

---
+++ app/Livewire/Pages/OrderSuccess.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Component;
+
+class OrderSuccess extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.order-success');
+    }
+}
